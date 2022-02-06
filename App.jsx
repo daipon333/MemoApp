8bd591ae6046b64fb7c1bc575f29b45630415324
daipon333 <dqllc0n@gmail.com>
2022-02-06 11:24:30 +0900

    Save memo on firestore

--- App.jsx
+++ App.jsx
@@ -14,6 +14,8 @@ import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 import LogInScreen from "./src/screens/LogInScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 
+require("firebase/firestore");
+
 const Stack = createStackNavigator();
 
 if (firebase.apps.length === 0) {
