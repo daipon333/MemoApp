be5e3642a9cbf10be5cec631018611ddb456ef75
daipon333 <dqllc0n@gmail.com>
2022-02-06 10:51:54 +0900

    Auto login and logout button

--- App.jsx
+++ App.jsx
@@ -23,7 +23,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="SignUp"
+        initialRouteName="LogIn"
         screenOptions={{
           headerStyle: {
             backgroundColor: "crimson",
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 
 import Memolist from "../components/MemoList";
 import CircleButton from "../components/CircleButton";
+import LogOutButton from "../components/LogOutButton";
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
+
   return (
     <TouchableOpacity style={styles.container}>
       <Memolist />
