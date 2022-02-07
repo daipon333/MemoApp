4cf448dfd7276b25184f08a306ffa7b51745e649
daipon333 <dqllc0n@gmail.com>
2022-02-07 22:29:30 +0900

    Create Loading Component

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { Alert, StyleSheet, TouchableOpacity } from "react-native";
+import { Alert, StyleSheet, View, Text } from "react-native";
 import firebase from "firebase";
 import Memolist from "../components/MemoList";
 import CircleButton from "../components/CircleButton";
 import LogOutButton from "../components/LogOutButton";
+import Button from "../components/Button";
+import Loading from "../components/Loading";
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
   const [memos, setMemos] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const db = firebase.firestore();
     const { currentUser } = firebase.auth();
     let unsubscribe = () => {};
     if (currentUser) {
+      setLoading(true);
       const ref = db
         .collection(`users/${currentUser.uid}/memos`)
         .orderBy("updatedAt", "desc");
@@ -30,9 +34,11 @@ export default function MemoListScreen(props) {
             });
           });
           setMemos(userMemos);
+          setLoading(false);
         },
         (error) => {
           console.log(error);
+          setLoading(false);
           Alert.alert("データの読み込みに失敗しました。");
         }
       );
@@ -47,8 +53,27 @@ export default function MemoListScreen(props) {
     });
   }, []);
 
+  if (memos.length === 0) {
+    return (
+      <View style={emptyStyles.container}>
+        <Loading isLoading={isLoading} />
+
+        <View style={emptyStyles.inner}>
+          <Text style={emptyStyles.title}>最初のメモを作成！</Text>
+          <Button
+            style={emptyStyles.button}
+            label="作成する"
+            onPress={() => {
+              navigation.navigate("MemoCreate");
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <View style={styles.container}>
       <Memolist memos={memos} />
       <CircleButton
         name="plus"
@@ -56,7 +81,7 @@ export default function MemoListScreen(props) {
           navigation.navigate("MemoCreate");
         }}
       />
-    </TouchableOpacity>
+    </View>
   );
 }
 
@@ -64,5 +89,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F0F4F8",
+  },
+});
+
+const emptyStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inner: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 24,
+  },
+  button: {
+    alignSelf: "center",
   },
 });
